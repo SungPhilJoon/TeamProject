255bbec65b8e77b53cd4ee68a99b55a8dd9eae25
soung5003@naver.com <soung5003@naver.com>
2022-04-05 15:33:32 +0900

    220405
    220405 meeting

---
+++ Assets/FeelJoon/Scripts/Test.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ETeam.FeelJoon
+{
+    public class Test : MonoBehaviour
+    {
+        #region Variables
+        private int numberOfState;
+
+        #endregion Variables
+
+        #region Properties
+        public int NumberOfState => numberOfState;
+
+        #endregion Properties
+
+        #region Helper Methods
+
+
+        #endregion Helper Methods
+    }
+}
